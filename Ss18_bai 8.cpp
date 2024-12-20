b7033de5776deacb18fd12508f719c31fc71ed0e
Admin <duong170226@gmail.com>
2024-12-20 15:09:32 +0700

    Add files via upload

---
+++ Ss18_bai 8.cpp
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//typedef int;
+typedef struct{
+	int id;
+	char name[30];
+	int age;
+	char tel[11];
+}student;
+void enter_list(student **sv,int *n){
+	printf("Enter the number of student :");
+	scanf("%d",n);
+	if(*n<1){
+		printf("Invalid number of student !\n");
+		exit(1);
+	}
+	getchar();
+	*sv=(student*)malloc(*n*sizeof(student));
+	if(*sv==NULL){
+		printf("Memory allocation failed !\n");
+		exit(1);
+	}
+	for(int i=0;i<*n;i++){
+		printf("Enter student 0%d:\n",i+1);
+		printf("Enter id:");
+		scanf("%d",&(*sv)[i].id);
+		getchar();
+		printf("Enter name :");
+		fgets((*sv)[i].name,30,stdin);
+		(*sv)[i].name[strcspn((*sv)[i].name,"\n")]='\0';
+		printf("Enter age:");
+		scanf("%d",&(*sv)[i].age);
+		getchar();
+		printf("Enter telephone number:");
+		fgets((*sv)[i].tel,11,stdin);
+		(*sv)[i].tel[strcspn((*sv)[i].tel,"\n")]='\0';
+		printf("\n");
+	}
+}
+
+void add_student(student **sv,int *n){
+	*n+=1;
+	*sv=(student*)realloc(*sv,*n*sizeof(student));
+	printf("Enter the location:");
+	int pos;
+	scanf("%d",&pos);
+	if(pos==0){
+		pos=1;
+	}
+	if(pos<1||pos>*n+1){
+		printf("Invalid location!\n");
+		return;
+	}
+	for(int i=*n;i>=pos;i--){
+		(*sv)[i]=(*sv)[i-1];
+	}
+	printf("New student:\n");
+	printf("Enter id:");
+	scanf("%d",&(*sv)[pos-1].id);
+	getchar();
+	printf("Enter name :");
+	fgets((*sv)[pos-1].name,30,stdin);
+	(*sv)[pos-1].name[strcspn((*sv)[pos-1].name,"\n")]='\0';
+	printf("Enter age:");
+	scanf("%d",&(*sv)[pos-1].age);
+	getchar();
+	printf("Enter telephone number:");
+	fgets((*sv)[pos-1].tel,11,stdin);
+	(*sv)[pos-1].tel[strcspn((*sv)[pos-1].tel,"\n")]='\0';
+	printf("\n");
+}
+void print_list(student *sv,int n){
+	printf("List of student:\n");
+	for(int i=0;i<n;i++){
+		printf("Id sudent %d:\n",sv[i].id);
+		printf("Name :%s\n",(*(sv+i)).name);
+		printf("Age :%d\n",(*(sv+i)).age);
+		printf("Tel :%s\n",(*(sv+i)).tel);
+		printf("\n");
+	}
+}
+int main(){
+	int n;
+	student *hs=NULL;
+	enter_list(&hs,&n);
+	add_student(&hs,&n);
+	print_list(hs,n);
+	free(hs);
+	return 0;
+}
